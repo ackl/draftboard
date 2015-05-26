61ee85ad951193f1a14040261c7ad0b1888125db
Paulo Ribeiro <pauloribs@gmail.com>
2015-05-26 15:45:12 +0000

    Moved Utility class to application module

--- application/Utility.py
+++ application/Utility.py
@@ -1,12 +1,7 @@
-from pymongo import MongoClient
 from flask import Response, make_response
 from bson.json_util import dumps
 
-def get_database():
-    client = MongoClient()
-    return client.dev_db
-
-def generate_response(query):
+def gen_response(query):
     response = Response(dumps(query), mimetype='application/json')
     return make_response(response)
 
